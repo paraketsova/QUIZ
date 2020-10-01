743bde4b08d9596701633e50a0276a5e646f3026
Mariia Paraketsova <paraketsova@gmail.com>
2020-10-01 18:35:29 +0200

    Add fetch for 5 and 1 qeustions.

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+
+//10 questions
 fetch('https://quizapi.io/api/v1/questions?apiKey=kQ640FJsMce9YQXnWD6fypSfdEBccAx3s71YzfAb&category=code&difficulty=Easy&limit=10')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+  
+//five question
+  fetch('https://quizapi.io/api/v1/questions?apiKey=kQ640FJsMce9YQXnWD6fypSfdEBccAx3s71YzfAb&category=code&difficulty=Easy&limit=5&tags=JavaScript')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+//one question
+  fetch('https://quizapi.io/api/v1/questions?apiKey=kQ640FJsMce9YQXnWD6fypSfdEBccAx3s71YzfAb&category=code&difficulty=Easy&limit=1&tags=JavaScript')
   .then((response) => response.json())
   .then((json) => console.log(json))
