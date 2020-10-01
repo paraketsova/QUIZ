682649be5705b2d11fc8b3632784f5b4a98957cc
Mariia Paraketsova <paraketsova@gmail.com>
2020-10-01 19:08:51 +0200

    Add js files (game, player, question)

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 
-//10 questions
-fetch('https://quizapi.io/api/v1/questions?apiKey=kQ640FJsMce9YQXnWD6fypSfdEBccAx3s71YzfAb&category=code&difficulty=Easy&limit=10')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+document.addEventListener("DOMContentLoaded", function(e) {
   
-//five question
-  fetch('https://quizapi.io/api/v1/questions?apiKey=kQ640FJsMce9YQXnWD6fypSfdEBccAx3s71YzfAb&category=code&difficulty=Easy&limit=5&tags=JavaScript')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+})
 
-//one question
-  fetch('https://quizapi.io/api/v1/questions?apiKey=kQ640FJsMce9YQXnWD6fypSfdEBccAx3s71YzfAb&category=code&difficulty=Easy&limit=1&tags=JavaScript')
-  .then((response) => response.json())
-  .then((json) => console.log(json))
