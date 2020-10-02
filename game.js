43c5c37ff8e00b70c7e18346cf9fb3059b887c92
Mariia Paraketsova <paraketsova@gmail.com>
2020-10-02 17:22:09 +0200

    Update class Game: add start() and play() metods.

--- game.js
+++ game.js
@@ -1,17 +1,31 @@
 class Game {
   constructor(){
-    let player = new Player(name);
-    let question = new Question();
-    let btnPlay = document.getElementById("btnPlay");
+    this.player = new Player(name);
+    this.question = new Question();
+    this.root = document.getElementById('root'); //так как используем в неск ф, то выносим её наверх и вместо константы она становится проперти данного класса.
+  }
+  start (event) {
+    const btnPlay = document.createElement('button'); //создаем кнопку как элемент 
 
+    btnPlay.innerHTML = 'Play now!'; 
+    btnPlay.addEventListener('click', (event) => { // вместо обычной ф мы пишем лямбда ф, которая позволяет ссылаться на внешнюю область видимости, так как иначе мы не можем писать её ведь у лямбды нет свойства this она по умолчанию ищет ее выше, на уровне класса.
+      this.play();
+    });
+    this.root.appendChild(btnPlay); // добавляем кнопку как потомка в див root
   }
 
+  play() { //игрок запустил игру, мы выводим диал окно с запросом имени и кол-ва вопросов квиза
+    console.log('ssss');
+    this.root.innerHTML = ''; //удаляем кнопку play now, теперь root пуст
+    const namesRoot = document.createElement('input'); //создаем input для ввода имени 
+    this.root.appendChild(namesRoot); 
+
+    const sizeQuizRoot = document.createElement('input'); //создаем input для определения кол-ва вопросов 
+    this.root.appendChild(sizeQuizRoot);
+
+
+  }
   
-  askName()  {
-    let name = 0;
-
-    return name;
-  }
   
   takeQuests_10 () {
     fetch('https://quizapi.io/api/v1/questions?apiKey=kQ640FJsMce9YQXnWD6fypSfdEBccAx3s71YzfAb&category=code&difficulty=Easy&limit=10')
